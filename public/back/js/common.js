de73cf5e25f0b1ef4c5d50057aef51540e194ee9
Wing <1531780174@qq.com>
2017-11-09 15:44:41 +0800

    所有页面的基本结构功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -5,8 +5,26 @@ $(document).ajaxStart(function(){
 });
 // 给所有ajax发送结束后加结束进度条
 $(document).ajaxStop(function(){
-    NProgress.done();
+    setTimeout(function(){
+        NProgress.done();
+    },500);
 });
+
+// 除登录页面之外,每个页面判断当前的登录状态,如果没有登录信息的就打回login.html
+var currentUrl = window.location.href;
+// 检索当前的url有没有"login.html"字符串存在
+if(currentUrl.indexOf('login.html')==-1){
+    // 判断登录状态
+    $.ajax({
+        url:"/employee/checkRootLogin",
+        success:function(backData){
+            // 是否未登录
+            if(backData.error==400){
+                location.href="login.html";
+            }
+        }
+    });
+}
 
 //分类管理切换开关
 $('.child').prev().on('click',function(){
@@ -19,4 +37,22 @@ $('.menu').on('click',function(){
     $('.aside_left').toggleClass('now');
     // 右边同时变大  操作类 toggleClass()
     $('.main_right').toggleClass('now');
+});
+
+//logout按钮退出登录
+$('.logout').on('click',function(){
+    // 弹出确认框
+    $('.modal').modal();
+    // 点击确认退出(因为on注册事件不会覆盖,所以事件嵌套时要用off()方法解绑)
+    $('.btn-primary').off().on('click',function(){
+        // 同时发送ajax,清除登录信息
+        $.ajax({
+            url:'/employee/employeeLogout',
+            success:function(backData){
+                // console.log(backData);
+            }
+        });
+        // 跳回login.html
+        location.href="login.html";        
+    });
 });
